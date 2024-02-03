8530f452d232bef197702d1bafecd26d2c0ebed5
KoenDR06 <koen.de.ruiter@hotmail.com>
2024-02-03 12:37:42 +0100

    Wrote program to add tags to website, which I forgot to select haha

---
+++ src/main/kotlin/me/koendev/pws/singleuse/RecipeToTagConversion.kt
@@ -0,0 +1,75 @@
+// todo fix this
+package me.koendev.pws.singleuse
+
+import io.ktor.server.plugins.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
+import me.koendev.pws.database.*
+import me.koendev.pws.dotEnv
+import me.koendev.pws.plugins.database
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+import println
+import readInput
+
+fun main() {
+    database = Database.connect(
+        url = dotEnv["DB_URL"],
+        user = dotEnv["DB_USER"],
+        driver = "org.mariadb.jdbc.Driver",
+        password = dotEnv["DB_PASSWORD"]
+    )
+
+    val recipeLoc = "static/new_recipe_data.json"
+    val JSONRecipes = Json.decodeFromString<List<JSONRecipe>>(readInput(recipeLoc).joinToString(""))
+
+    val recipes: MutableMap<String, Int> = mutableMapOf()
+    var t = 0
+    transaction {
+        for (recipe in JSONRecipes) {
+            for (i in 1..5044) {
+                if (recipes[recipe.id] == null) {
+                    if (RecipeItem.findById(i)?.title == recipe.title) {
+                        recipes[recipe.id] = i
+                        println(++t)
+                        break
+                    }
+                }
+            }
+        }
+    }
+
+    val duplicates: MutableList<Int?> = mutableListOf()
+    val recipesAdded = mutableListOf<Int>()
+    runBlocking {
+        val recipeTagDB = RecipeTagsService(database = database)
+        t = 0
+        for (recipe in JSONRecipes) {
+            if(recipe.id.substring(3).toInt() !in recipesAdded) {
+                recipesAdded.add(recipe.id.substring(3).toInt())
+                for (tag in recipe.tags) {
+                    val tagId = recipeTagDB.dbQuery {
+                        TagsService.Tags.select { TagsService.Tags.name eq tag }
+                            .map { it[TagsService.Tags.id] }
+                            .singleOrNull()
+                    } ?: throw NotFoundException("Tag was not found in database.")
+                    try {
+                        recipeTagDB.create(RecipeTag(recipes[recipe.id]!!, tagId = tagId.value))
+                    } catch (e: Exception) {
+                        println("Recipe with ID ${recipes[recipe.id]} is a duplicate.")
+                        duplicates.add(recipes[recipe.id])
+                    }
+                }
+
+            }
+            if (t % 100 == 0) {
+                println(t++)
+            } else {
+                ++t
+            }
+
+        }
+    }
+    duplicates.println()
+}
