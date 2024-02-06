616421e359ff07cb1f16d8de87cb69c88a104927
KoenDR06 <koen.de.ruiter@hotmail.com>
2024-02-06 13:46:29 +0100

    Started work on some Status Pages

--- src/main/kotlin/me/koendev/pws/plugins/Routing.kt
+++ src/main/kotlin/me/koendev/pws/plugins/Routing.kt
@@ -2,12 +2,14 @@ package me.koendev.pws.plugins
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.html.*
 import io.ktor.server.http.content.*
 import io.ktor.server.plugins.doublereceive.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.html.*
 import me.koendev.pws.api.apiRouting
 import me.koendev.pws.site.webRouting
 
@@ -15,8 +17,27 @@ fun Application.configureRouting() {
     install(DoubleReceive)
     install(Resources)
     install(StatusPages) {
-        exception<Throwable> { call, cause ->
-            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
+        status(HttpStatusCode.NotFound) { call, status ->
+            call.respondHtml(HttpStatusCode.NotFound) {
+                head {
+                    title {
+                        +"Pagina niet gevonden..."
+                    }
+                }
+                body {
+                    h1 {
+                        +"Helaas hebben we deze pagina niet kunnen vinden."
+                    }
+                    p {
+                        +"Klik hieronder op de knop om naar de homepagina te gaan."
+                    }
+                    form(action = "/") {
+                        input(type = InputType.submit) {
+                            value = "Homepagina"
+                        }
+                    }
+                }
+            }
         }
     }
     routing {
